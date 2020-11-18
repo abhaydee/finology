7ae143397e081333bff6b5e9cee7bdf1eb6b526c
Girish Deshpande <a.a.girish.deshpande@accenture.com>
2020-11-18 21:30:35 +0530

    intializing navbar

--- src/Components/NavContainer/NavContainer.jsx
+++ src/Components/NavContainer/NavContainer.jsx
@@ -2,14 +2,21 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 import Logo from "../../assets/images/logo.png"
 import "./NavContainer.scss"
+import {navList} from "../../utils/utils"
 function NavContainer() {
     return (
         <Container>
             <div className="nav-container">
                <img src={Logo} width={70} height={70} className="nav-container__img"/>
-               <div>
-                   
-               </div>
+               <ul>
+                {navList.map((data)=>(
+                    <li key={data.id} className="nav-container__listitem">
+                        {data.title}
+                    </li>
+                ))
+                }
+               </ul>
+               
             </div>
         </Container>
     )
---
+++ src/utils/utils.js
@@ -0,0 +1,7 @@
+export let navList=[
+    {id:"1",title:"Home"},
+    {id:"2",title:"About Us"},
+    {id:"3",title:"Services"},
+    {id:"4",title:"Pricing"},
+    {id:"5",title:"Careers"}
+]
