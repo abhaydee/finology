293c2b5c75fbcbe9c9148ffb1e59f1038412ffe1
Girish Deshpande <a.a.girish.deshpande@accenture.com>
2020-11-18 22:53:47 +0530

    updates to banner

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,6 +6,13 @@ function Header() {
     return (
         <Container fluid className="header">
             <NavContainer/>
+            <Container className="header__data">
+                <div>
+                    <h3 className="header__title">Make development easy with us</h3>
+                    <p className="header__description"> Doing development can never be easy and it takes time and resources. We at EasyWork has the solution </p>
+                </div>
+                
+            </Container>
         </Container>
     )
 }
--- src/Components/NavContainer/NavContainer.jsx
+++ src/Components/NavContainer/NavContainer.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
 import Logo from "../../assets/images/logo.png"
+import Search from "../../assets/icons/search.svg"
 import "./NavContainer.scss"
 import {navList} from "../../utils/utils"
 function NavContainer() {
     return (
         <Container>
             <div className="nav-container">
-               <img src={Logo} width={70} height={70} className="nav-container__img"/>
-               <ul>
+               <img src={Logo} width={50} height={50} className="nav-container__img"/>
+               <ul className="nav-container__list">
                 {navList.map((data)=>(
                     <li key={data.id} className="nav-container__listitem">
                         {data.title}
@@ -16,7 +17,7 @@ function NavContainer() {
                 ))
                 }
                </ul>
-               
+                <img src={Search} className="nav-container__search"  width={30} height={30}/>
             </div>
         </Container>
     )
