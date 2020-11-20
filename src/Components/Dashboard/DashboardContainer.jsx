96a74521e7b72a539bb50f2069315be8e2db8818
Girish Deshpande <a.a.girish.deshpande@accenture.com>
2020-11-20 16:19:35 +0530

    changes to signin container

--- src/Components/Dashboard/DashboardContainer.jsx
+++ src/Components/Dashboard/DashboardContainer.jsx
@@ -1,27 +1,45 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
-import "./DashboardContainer.scss"
-import Dashboard from "../../assets/images/dashboard.png"
-import Signin from "../../assets/images/sign-in.png"
+import React from "react";
+import { Container } from "react-bootstrap";
+import "./DashboardContainer.scss";
+import Dashboard from "../../assets/images/dashboard.png";
+import Signin from "../../assets/images/sign-in.png";
 function DashboardContainer() {
-    return (
-        <Container className="dashboard-container">
-            <div>
-            <img src={Dashboard} className="dashboard-container__image"/>
-            <div className="dashboard-container__content">
-                <h3 className="dashboard-container__title">We can give you our best user experience to your system</h3>
-                <p className="dashboard-container__description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quibusdam dignissimos, suscipit quae voluptatum ipsa mollitia odit! Possimus numquam minus sit, dolor perspiciatis nesciunt suscipit neque consectetur voluptas vero alias? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit, molestias! Unde, repellendus voluptate tenetur repudiandae perspiciatis excepturi ullam laboriosam, vitae distinctio est maxime sequi tempore ducimus, eaque fugit quam velit?</p>
-            </div>
-            </div>
-            <div>
-            <div className="dashboard-container__content">
-                <h3 className="dashboard-container__title">We can give you our best user experience to your system</h3>
-                <p className="dashboard-container__description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quibusdam dignissimos, suscipit quae voluptatum ipsa mollitia odit! Possimus numquam minus sit, dolor perspiciatis nesciunt suscipit neque consectetur voluptas vero alias? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit, molestias! Unde, repellendus voluptate tenetur repudiandae perspiciatis excepturi ullam laboriosam, vitae distinctio est maxime sequi tempore ducimus, eaque fugit quam velit?</p>
-            </div>
-            <img src={Signin} className="dashboard-container__image"/>
-            </div>
-        </Container>
-    )
+  return (
+    <Container>
+      <div className="dashboard-container">
+        <img src={Dashboard} className="dashboard-container__image" />
+        <div className="dashboard-container__content">
+          <h3 className="dashboard-container__title">
+            We can give you our best user experience to your system
+          </h3>
+          <p className="dashboard-container__description">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+            quibusdam dignissimos, suscipit quae voluptatum ipsa mollitia odit!
+            Possimus numquam minus sit, dolor perspiciatis nesciunt suscipit
+            neque consectetur voluptas vero alias? Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Fugit, molestias! Unde, repellendus
+            tur, adipisicing elit. Fugit, moles
+          </p>
+        </div>
+      </div>
+      <div className="signin-container">
+          <div className="dashboard-container__content">
+            <h3 className="dashboard-container__title">
+              We can give you our best user experience to your system
+            </h3>
+            <p className="dashboard-container__description">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+            quibusdam dignissimos, suscipit quae voluptatum ipsa mollitia odit!
+            Possimus numquam minus sit, dolor perspiciatis nesciunt suscipit
+            neque consectetur voluptas vero alias? Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Fugit, molestias! Unde, repellendus
+            tur, adipisicing elit. Fugit, moles
+            </p>
+          </div>
+          <img src={Signin} className="dashboard-container__image" />
+      </div>
+    </Container>
+  );
 }
 
 export default DashboardContainer;
