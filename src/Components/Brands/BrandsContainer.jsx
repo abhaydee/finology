c559453904a3fd620746fe5842c6b5b83a289867
Girish Deshpande <a.a.girish.deshpande@accenture.com>
2020-11-21 15:07:58 +0530

    ui updates to brand container

--- src/Components/Brands/BrandsContainer.jsx
+++ src/Components/Brands/BrandsContainer.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
 import Heading from '../Heading/Heading'
-
+import {Partners} from "../../utils/utils"
+import styles from "./BrandsContainer.module.scss"
 function BrandsContainer() {
     return (
-        <div className="brands-container">
+        <div className={styles["brands-container"]}>
             <Heading heading="We've worked with"/>
+            <Container>
+                {Partners.map((brand)=>(
+                    <img key={brand.id} src={brand.imageurl}  className={styles["brands-container__image"]}  />
+                ))}
+            </Container>  
         </div>
     )
 }
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -6,6 +6,16 @@ export let navList=[
     {id:"5",title:"Careers"}
 ]
 
+export const Partners=[{id:"1",imageurl:require("../assets/images/apple.PNG").default},
+{id:"2",imageurl:require("../assets/images/airbnb.png").default},
+{id:"3",imageurl:require("../assets/images/google.PNG").default},
+{id:"4",imageurl:require("../assets/images/nvidia.PNG").default},
+{id:"5",imageurl:require("../assets/images/tesla.PNG").default},
+{id:"6",imageurl:require("../assets/images/samsung.PNG").default},
+{id:"7",imageurl:require("../assets/images/facebook.PNG").default},
+{id:"8",imageurl:require("../assets/images/microsoft.PNG").default},
+]
+
 export const componentCheckSize=()=>{
     let isMobile=false
     if(window.outerWidth<768){
