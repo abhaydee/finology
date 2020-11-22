f6647731f779f2d92f59f7f6b59125ecd83b8af1
abhaydee <abhaygirish1997@gmail.com>
2020-11-23 01:12:04 +0530

    ui updates to carousel

--- src/Components/Service/Service.jsx
+++ src/Components/Service/Service.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import Heart from "../../assets/icons/heart.svg"
-import {componentCheckSize} from "../../utils/utils";
+import {componentCheckSize,service} from "../../utils/utils";
 import Heading from "../Heading/Heading";
-import styles from "./Service.module.scss";
 import ServiceCard from "./ServiceCard";
+import Carousel from "react-elastic-carousel"
+import styles from "./Service.module.scss";
+
 function Service() {
     const isMobile=componentCheckSize();
   return (
@@ -20,7 +22,13 @@ function Service() {
         />
       </div>
     </Container>
-    <ServiceCard/>
+    <Carousel itemsToShow={isMobile? 1:2.5}>
+        {service.map((serve)=>(
+            <div id={serve.id}>
+            <ServiceCard imageurl={serve.imageurl} title={serve.title} profession={serve.profession} description={serve.description}/>
+            </div>
+        ))}
+    </Carousel>
     </>
   );
 }
--- src/Components/Service/ServiceCard.jsx
+++ src/Components/Service/ServiceCard.jsx
@@ -4,20 +4,20 @@ import {service} from "../../utils/utils";
 import Avatar from "../../assets/people/darlene-chabrat.jpg";
 import styles from "./ServiceCard.module.scss"
 import Heading from '../Heading/Heading';
-function ServiceCard({}) {
+function ServiceCard({imageurl,title,description,profession}) {
     return (
         <div className={styles["servicecard"]}>
             <Container>
                 <div className={styles["servicecard__container"]}>
-                    <img src={Avatar}  className={styles["servicecard__image"]}/>
+                    <img src={imageurl}  className={styles["servicecard__image"]}/>
                     <div className={styles["servicecard__content"]}>
-                        <Heading heading={"David Campion"} className={styles["servicecard__title"]}/>
-                        <Heading heading={"CEO of Icloud"} className={styles["servicecard__subtitle"]}/>
+                        <Heading heading={title} className={styles["servicecard__title"]}/>
+                        <Heading heading={profession} className={styles["servicecard__subtitle"]}/>
                     </div>
                 </div>
                 <div>
                 <div className={styles["servicecard__description"]}>
-                        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, quo."
+                        {description}
                     </div>
                 </div>
             </Container>
