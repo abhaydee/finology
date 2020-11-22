aed484e641f1c0f757231890bec2b70994ea30fc
abhaydee <abhaygirish1997@gmail.com>
2020-11-22 23:30:24 +0530

    changes to service ui

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import BrandsContainer from './Components/Brands/BrandsContainer';
 import Footer from './Components/Footer/Footer';
 import Contact from './Components/Contact/Contact';
 import People from './Components/People/People';
+import Service from './Components/Service/Service';
 function App() {
   return (
     <div className="App">
       <Header/>
       <DashboardContainer/>
+      <Service/>
       <People/>
       <BrandsContainer/>
       <Contact/>
--- src/Components/Service/Service.jsx
+++ src/Components/Service/Service.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-import "./Service.module.scss"
+import React from "react";
+import { Container } from "react-bootstrap";
+import Heart from "../../assets/icons/heart.svg"
+import {componentCheckSize} from "../../utils/utils";
+import Heading from "../Heading/Heading";
+import styles from "./Service.module.scss";
 function Service() {
-    return (
-        <div>
-            
-        </div>
-    )
+    const isMobile=componentCheckSize();
+  return (
+    <>
+    <Container className={styles["service"]}>
+      <div className={styles["service__heart"]}>
+        {!isMobile && (
+          <img src={Heart} className={styles["service__image"]} />
+        )}
+        <Heading
+          heading={"What other people say about our service"}
+          className={styles["service__heading"]}
+        />
+      </div>
+    </Container>
+    </>
+  );
 }
 
-export default Service
+export default Service;
