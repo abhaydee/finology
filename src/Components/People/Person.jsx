8803376ba37cd1beabdb2b49e0511b4656bd4be9
abhaydee <abhaygirish1997@gmail.com>
2020-11-22 19:45:13 +0530

    ui updates v2 to cards

--- src/Components/People/Person.jsx
+++ src/Components/People/Person.jsx
@@ -13,10 +13,9 @@ function Person() {
               className={styles["person__image"]}
             />
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+              <Card.Title className={styles["person__title"]}>{person.title}</Card.Title>
+              <Card.Text className={styles["person__description"]}>
+                {person.description}
               </Card.Text>
             </Card.Body>
           </Card>
