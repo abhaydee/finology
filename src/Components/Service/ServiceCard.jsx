1b7822c4c9bb9e5958ec14267f5e60a6becb8562
abhaydee <abhaygirish1997@gmail.com>
2020-11-22 23:37:50 +0530

    initialising service cards

---
+++ src/Components/Service/ServiceCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "./ServiceCard.module.scss"
+function ServiceCard() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ServiceCard;
