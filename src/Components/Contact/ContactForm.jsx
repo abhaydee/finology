46b125c2f031ded101be2245e1fee125df6e1a5f
abhaydee <abhaygirish1997@gmail.com>
2020-11-23 23:41:39 +0530

    modularizing code

---
+++ src/Components/Contact/ContactForm.jsx
@@ -0,0 +1,77 @@
+import React,{useState} from 'react'
+import { Container, Form } from "react-bootstrap";
+import styles from "./Contact.module.scss";
+function ContactForm() {
+    const [input,setInput]=useState({});
+    const handleChange=(e)=>{
+        setInput({
+            ...input,
+            [e.target.name]:e.target.value
+        })
+    }
+    console.log("the input details",input)
+    return (
+        <Form className={styles["contact__container"]}>
+        <Form.Group
+          controlId="formBasicEmail"
+          className={styles["contact__group"]}
+        >
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter Name" onChange={handleChange} name="Contact Name"/>
+          <Form.Control.Feedback
+            type="invalid"
+            className={styles["contact__error"]}
+          >
+            Please Enter your Name
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group
+          controlId="formBasicEmail"
+          className={styles["contact__group"]}
+        >
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Enter Email" onChange={handleChange} name="Email"/>
+          <Form.Control.Feedback
+            type="invalid"
+            className={styles["contact__error"]}
+          >
+            Please Enter your email
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group
+          controlId="formBasicEmail"
+          className={styles["contact__group"]}
+        >
+          <Form.Label>Subject</Form.Label>
+          <Form.Control type="text" placeholder="Enter Subject"  onChange={handleChange} name="Contact Subject"/>
+          <Form.Control.Feedback
+            type="invalid"
+            className={styles["contact__error"]}
+          >
+            Please Enter your Subject
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group
+          controlId="formBasicEmail"
+          className={styles["contact__group"]}
+        >
+          <Form.Label>Message</Form.Label>
+          <Form.Control as="textarea" aria-label="With textarea" onChange={handleChange} name="Message" placeholder="Enter your message here"/>
+          <Form.Control.Feedback
+            type="invalid"
+            className={styles["contact__error"]}
+          >
+            Please Enter your Message
+          </Form.Control.Feedback>
+        </Form.Group>
+        <input
+          type="submit"
+          name="Send"
+          value="Send"
+          className={styles["contact__button"]}
+        />
+      </Form>
+    )
+}
+
+export default ContactForm
--- src/Components/People/PeopleModal.jsx
+++ src/Components/People/PeopleModal.jsx
@@ -1,25 +1,43 @@
-import React from 'react'
-import {Modal,Button} from "react-bootstrap"
-import styles from "./PeopleModal.module.scss"
-function PeopleModal({toggleModal,handleClose}) {
-    return (
-        <Modal show={toggleModal}
-        onHide={handleClose}
-        dialogClassName={styles["modal"]}
+import React from "react";
+import { Modal, Button } from "react-bootstrap";
+import User from "../../assets/icons/user.svg";
+import styles from "./PeopleModal.module.scss";
+import styles1 from "../Contact/Contact.module.scss";
+import ContactForm from "../Contact/ContactForm";
+function PeopleModal({ toggleModal, handleClose }) {
+  return (
+    <Modal
+      show={toggleModal}
+      onHide={handleClose}
+      dialogClassName={styles["modal"]}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>Modal Heading</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <img src={User} className={styles["modal__image"]} />
+        <div className={styles1["contact"]}> 
+        <ContactForm  message={"description"} labelReq={"position"}/>
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button
+          variant="secondary"
+          className={styles["modal__cancel"]}
+          onClick={handleClose}
         >
-        <Modal.Header closeButton >
-          <Modal.Title>Modal Heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-            
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    )
+          Cancel
+        </Button>
+        <Button
+          variant="secondary"
+          className={styles["modal__save"]}
+          onClick={handleClose}
+        >
+          Save
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
 }
 
-export default PeopleModal
+export default PeopleModal;
