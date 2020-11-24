a22449327db59673fbe68d953777769e6e17504c
abhaydee <abhaygirish1997@gmail.com>
2020-11-24 18:44:38 +0530

    code refactoring v3

--- src/Components/Contact/ContactForm.jsx
+++ src/Components/Contact/ContactForm.jsx
@@ -1,80 +1,101 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 import { Container, Form } from "react-bootstrap";
 import styles from "./Contact.module.scss";
 function ContactForm(props) {
-    const [input,setInput]=useState({});
-    const handleChange=(e)=>{
-        setInput({
-            ...input,
-            [e.target.name]:e.target.value
-        })
-    }
-    return (
-        <Form className={styles["contact__container"]}>
-        <Form.Group
-          controlId="formBasicEmail"
-          className={styles["contact__group"]}
-        >
-          <Form.Label>Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter Name" onChange={handleChange} name="Contact Name"/>
-          <Form.Control.Feedback
-            type="invalid"
-            className={styles["contact__error"]}
-          >
-            Please Enter your Name
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group
-          controlId="formBasicEmail"
-          className={styles["contact__group"]}
-        >
-          <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Enter Email" onChange={handleChange} name="Email"/>
-          <Form.Control.Feedback
-            type="invalid"
-            className={styles["contact__error"]}
-          >
-            Please Enter your email
-          </Form.Control.Feedback>
-        </Form.Group>
-       {props.subjectReq &&
-        <Form.Group
+  const [input, setInput] = useState({});
+  const handleChange = (e) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };
+  return (
+    <Form className={styles["contact__container"]}>
+      <Form.Group
         controlId="formBasicEmail"
         className={styles["contact__group"]}
       >
-        <Form.Label>Subject</Form.Label>
-        <Form.Control type="text" placeholder="Enter Subject"  onChange={handleChange} name="Contact Subject"/>
+        <Form.Label>Name</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter Name"
+          onChange={handleChange}
+          name="Contact Name"
+        />
         <Form.Control.Feedback
           type="invalid"
           className={styles["contact__error"]}
         >
-          Please Enter your Subject
+          Please Enter your Name
         </Form.Control.Feedback>
-      </Form.Group>}
+      </Form.Group>
+      <Form.Group
+        controlId="formBasicEmail"
+        className={styles["contact__group"]}
+      >
+        <Form.Label>Email</Form.Label>
+        <Form.Control
+          type="email"
+          placeholder="Enter Email"
+          onChange={handleChange}
+          name="Email"
+        />
+        <Form.Control.Feedback
+          type="invalid"
+          className={styles["contact__error"]}
+        >
+          Please Enter your email
+        </Form.Control.Feedback>
+      </Form.Group>
+      {props.subjectReq && (
         <Form.Group
           controlId="formBasicEmail"
           className={styles["contact__group"]}
         >
-          <Form.Label>{props.message}</Form.Label>
-          <Form.Control as="textarea" aria-label="With textarea" onChange={handleChange} name={props.message} placeholder="Enter your message here"/>
+          <Form.Label>Subject</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter Subject"
+            onChange={handleChange}
+            name="Contact Subject"
+          />
           <Form.Control.Feedback
             type="invalid"
             className={styles["contact__error"]}
           >
-            Please Enter your Message
+            Please Enter your Subject
           </Form.Control.Feedback>
         </Form.Group>
-        {props.buttonReq &&
-            <input
-            type="submit"
-            name="Send"
-            value="Send"
-            className={styles["contact__button"]}
-          />
-        }
-        
-      </Form>
-    )
+      )}
+      <Form.Group
+        controlId="formBasicEmail"
+        className={styles["contact__group"]}
+      >
+        <Form.Label>{props.message}</Form.Label>
+        <Form.Control
+          as="textarea"
+          aria-label="With textarea"
+          onChange={handleChange}
+          name={props.message}
+          placeholder="Enter your message here"
+        />
+        <Form.Control.Feedback
+          type="invalid"
+          className={styles["contact__error"]}
+        >
+          Please Enter your Message
+        </Form.Control.Feedback>
+      </Form.Group>
+      {props.buttonReq && (
+        <input
+          type="submit"
+          name="Send"
+          value="Send"
+          className={styles["contact__button"]}
+        />
+      )}
+    </Form>
+  );
 }
 
-export default ContactForm
+export default ContactForm;
--- src/Components/Contact/ContactMap.jsx
+++ src/Components/Contact/ContactMap.jsx
@@ -1,23 +1,29 @@
-import React from 'react'
+import React from "react";
 import GoogleMapReact from "google-map-react";
-import {ComponentReSize} from "../../utils/utils"
-import styles from "./Contact.module.scss"
+import { ComponentReSize } from "../../utils/utils";
+import styles from "./Contact.module.scss";
 function ContactMap() {
-    const isMobile=ComponentReSize();
-    const center={
-        lat:59.95,
-        lng:30.33,
-    }
-    const zoom=11;
-    return (
-        <div style={{height: '450px', width: isMobile?"100%":"450px",marginTop:"2rem"}}>
-             <GoogleMapReact
-          bootstrapURLKeys={{ key: "AIzaSyDjBGjBQFm1WHlUvLDvcsqMeLVlK9SMO4E"}}
-          defaultCenter={center}
-          defaultZoom={zoom}
-        ></GoogleMapReact>
-        </div>
-    )
+  const isMobile = ComponentReSize();
+  const center = {
+    lat: 59.95,
+    lng: 30.33,
+  };
+  const zoom = 11;
+  return (
+    <div
+      style={{
+        height: "450px",
+        width: isMobile ? "100%" : "450px",
+        marginTop: "2rem",
+      }}
+    >
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyDjBGjBQFm1WHlUvLDvcsqMeLVlK9SMO4E" }}
+        defaultCenter={center}
+        defaultZoom={zoom}
+      ></GoogleMapReact>
+    </div>
+  );
 }
 
-export default ContactMap
+export default ContactMap;
